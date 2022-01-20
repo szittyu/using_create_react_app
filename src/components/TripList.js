a943c668269e3a6831dca40a611d6b79c4857285
szittyu <szittyu1@gmail.com>
2022-01-20 18:47:31 +0100

    finished task

---
+++ src/components/TripList.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { useFetch } from "../hooks/useFetch"
+
+//styles
+import "./TripList.css"
+
+export default function TripList() {
+    const [url,setUrl] = useState("http://localhost:3000/trips")
+    const { data: trips, isPending, error } = useFetch(url, {type: "GET"})  
+
+
+  return (
+    <div className="trip-list">
+        <h2>Trip list</h2>
+        {isPending && <div>Loading trips...</div>}
+        {error && <div>{error}</div>}
+        <ul>
+            {trips && trips.map(trip => (
+                <li key={trip.id}>
+                    <h3>{trip.title}</h3>
+                    <p>{trip.price}</p>
+                </li>
+            ))}
+        </ul>
+        <div className="filters">
+            <button onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}>
+                Europian trips
+            </button>
+            <button onClick={() => setUrl("http://localhost:3000/trips")}>
+                All trips
+            </button>
+        </div>
+    </div>
+  )
+}
